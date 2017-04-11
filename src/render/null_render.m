943b3bb39615799a2c08d6e41ec478963e8f4eea
FlyingJester <foolkingcrown@gmail.com>
2017-04-10 20:21:23 -0700

    Add null renderer and window backend

---
+++ src/render/null_render.m
@@ -0,0 +1,75 @@
+:- module null_render.
+%==============================================================================%
+% Null renderer, swallows all input. Mainly used for testing purposes, mostly
+% finding if a crash is OpenGL or Mercury.
+:- interface.
+%==============================================================================%
+
+:- use_module render.
+
+:- type null_render.
+:- type null_model.
+:- type null_texture.
+:- type null_heightmap.
+
+:- instance render.render(null_render).
+:- instance render.model_compiler(null_render, null_model).
+:- instance render.model(null_render, null_model).
+:- instance render.skybox(null_render, null_texture).
+:- instance render.heightmap(null_render, null_heightmap, null_texture).
+
+%==============================================================================%
+:- implementation.
+%==============================================================================%
+
+:- type null_render ---> null_render.
+:- type null_model ---> null_model.
+:- type null_texture ---> null_texture.
+:- type null_heightmap ---> null_heightmap.
+
+:- instance render.render(null_render) where [
+    frustum(null_render, _, _, _, _, _, _, !IO),
+    
+    enable_depth(null_render, !IO),
+    
+    push_matrix(null_render, !IO),
+    pop_matrix(null_render, !IO),
+    
+    translate(null_render, _, _, _, !IO),
+    
+    rotate_x(null_render, _, !IO),
+    rotate_y(null_render, _, !IO),
+    rotate_z(null_render, _, !IO),
+    rotate_z(null_render, _, !IO),
+    
+    scale(null_render, _, _, _, !IO),
+
+    ( max_lights = 0),
+
+    light(null_render, _, _, !IO)
+].
+
+%------------------------------------------------------------------------------%
+
+:- instance model_compiler(null_render, null_model) where [
+    compile_wavefront(_, null_render, null_model),
+    compile_softshape(_, null_render, null_model)
+].
+
+%------------------------------------------------------------------------------%
+
+:- instance model(null_render, null_model) where [
+    draw(null_render, null_model, !IO)
+].
+
+%------------------------------------------------------------------------------%
+
+:- instance skybox(null_render, null_texture) where [
+    draw_skybox(null_render, _, _, null_texture, !IO)
+].
+
+%------------------------------------------------------------------------------%
+
+:- instance heightmap(null_render, null_heightmap, null_texture) where [
+    draw_heightmap(null_render, null_heightmap, null_texture, !IO)
+].
---
+++ src/window/null_window.m
@@ -0,0 +1,92 @@
+:- module null_window.
+%==============================================================================%
+% Null window, does nothing. Mainly used for testing purposes, mostly finding if
+% a crash is in Glow/FLTK or Mercury.
+:- interface.
+%==============================================================================%
+
+:- use_module io.
+:- use_module maybe.
+:- use_module window.
+
+%------------------------------------------------------------------------------%
+
+:- type window.
+:- type context.
+
+%------------------------------------------------------------------------------%
+
+:- pred create_window(int, int, string, window.gl_version, window, io.io, io.io).
+:- mode create_window(in, in, in, in, uo, di, uo) is det.
+:- mode create_window(in, in, in, di, uo, di, uo) is det.
+:- mode create_window(di, di, in, in, uo, di, uo) is det.
+:- mode create_window(di, di, in, di, uo, di, uo) is det.
+
+:- pred create_window(int, int, string, int, int, window, io.io, io.io).
+:- mode create_window(in, in, in, in, in, uo, di, uo) is det.
+
+:- func context(window) = context.
+
+:- pred hide(window, io.io, io.io).
+:- mode hide(in, di, uo) is det.
+
+:- pred show(window, io.io, io.io).
+:- mode show(in, di, uo) is det.
+
+:- pred title(window, string, io.io, io.io).
+:- mode title(in, in, di, uo) is det.
+
+:- pred size(window, int, int, io.io, io.io).
+:- mode size(in, uo, uo, di, uo) is det.
+
+:- pred wait(window, window.window_event, io.io, io.io).
+:- mode wait(in, uo, di, uo) is det.
+
+:- pred check(window, maybe.maybe(window.window_event), io.io, io.io).
+:- mode check(in, uo, di, uo) is det.
+
+:- pred make_current(context::in, io.io::di, io.io::uo) is det.
+
+%------------------------------------------------------------------------------%
+
+:- instance window.window(window).
+:- instance window.gl_context(context).
+
+%==============================================================================%
+:- implementation.
+%==============================================================================%
+
+:- type window ---> window.
+:- type context ---> context.
+
+create_window(_, _, _, _, window, !IO).
+create_window(_, _, _, _, _, window, !IO).
+
+context(_) = context.
+
+hide(window, !IO).
+show(window, !IO).
+
+title(window, _, !IO).
+
+size(window, 1, 1, !IO).
+
+wait(window, window.resize(1, 1), !IO).
+
+check(window, maybe.no, !IO).
+
+make_current(context, !IO).
+
+:- instance window.window(window) where [
+    pred(window.show/3) is null_window.show,
+    pred(window.hide/3) is null_window.hide,
+    pred(window.title/4) is null_window.title,
+    pred(window.size/5) is null_window.size,
+    pred(window.wait/4) is null_window.wait,
+    pred(window.check/4) is null_window.check,
+    pred(window.run/5) is window.run_basic
+].
+
+:- instance window.gl_context(context) where [
+    pred(window.make_current/3) is null_window.make_current
+].
